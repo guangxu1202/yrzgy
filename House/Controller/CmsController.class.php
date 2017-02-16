e296dd4fe218aa14512e9e82052097a37c0bf383
guangxu1202 <guangxu1202@sina.com>
2017-02-16 17:17:51 +0800

    company 0216

--- House/Controller/CmsController.class.php
+++ House/Controller/CmsController.class.php
@@ -214,6 +214,7 @@ class CmsController extends CommonController
             $this -> display();
         }
     }
+
     //文章过时与恢复
     function articleLocked(){
         if (I('get.sa')=="lock"){
@@ -257,4 +258,268 @@ class CmsController extends CommonController
         $this -> assign("info",$info);
         $this -> display();
     }
+
+    //过时与恢复
+    function personLocked(){
+        if (I('get.sa')=="lock"){
+            $str = "恭喜您，记录过时成功！";
+            $sa = false;
+        }else{
+            $str = "恭喜您，记录恢复成功！";
+            $sa = true;
+        }
+        $model = M("person_intro");
+        $data = array('is_show'=> $sa,'regenerator'=>session("a_username"),'update_time'=>date("Y-m-d H:i:s"));
+        $model-> where('pk='.I('get.pa'))->setField($data);
+
+        //操作成功
+        $this->success($str,__MODULE__."/Cms/personList");
+    }
+
+    //新增
+    function personAdd(){
+        if (!empty($_POST)){
+            //实例化
+            $model = new \Model\Person_introModel();
+            //验证数据 Person_introModel
+            $z = $model -> create();
+            if (!$z){
+                //show_bug($model -> getError());
+                $this->error("您录入的数据格式错误！");
+                exit();
+            }
+
+
+            //上传照片处理
+            $upload = new \Think\Upload();// 实例化上传类
+            $upload->maxSize   =     3145728 ;// 设置附件上传大小
+            $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+            $upload->rootPath  =      'CDN/uploaded/person/'; // 设置附件上传根目录
+            $upload->autoSub  =      true; 
+            // 上传单个文件
+            $info   =   $upload->uploadOne($_FILES['photo']);
+            if(!$info) {// 上传错误提示错误信息
+                $this->error($upload->getError());
+            }else{// 上传成功 获取上传文件信息
+                $photo =  $info['savepath'].$info['savename'];
+                $thumb = $info['savepath'].'thumb_'.$info['savename'];
+
+                //缩略图处理
+                $image = new \Think\Image();
+                $image->open('CDN/uploaded/person/'.$photo);
+                // 生成缩略图
+                $image->thumb(110, 90)->save('CDN/uploaded/person/'.$thumb);
+            }
+
+            //数据录入
+            $log["create_time"] = date("Y-m-d H:i:s");
+            $log["creator"] = session("a_username");
+            $log["update_time"] = date("Y-m-d H:i:s");
+            $log["regenerator"] = session("a_username");
+            $log["allow_copy"] = checkBit(I('post.allow_copy'));
+            $log["author"] = I("post.author");
+            $log["content"] = I("post.content");
+            $log["custom_sort"] = I("post.custom_sort");
+            $log["keywords"] = I("post.keywords");
+            $log["photo_describe"] = I("post.photo_describe");
+            $log["is_show"] = true;
+            $log["summary"] = I("post.summary");
+            $log["title"] = I("post.title");
+            $log["browse_count"] = 0;
+            $log["photo"] = $photo;
+            $log["thumbnail"] = $thumb;
+            $model->data($log)->add();
+
+            //录入成功
+            $this->success("恭喜您，操作成功！",__MODULE__."/Cms/personList");
+
+        }else{
+            $this -> display();
+        }
+    }
+
+    //修改
+    function personEdit(){
+        if (!empty($_POST)){
+            $model = new \Model\Person_introModel();
+            //验证数据 Person_introModel
+            $z = $model -> create();
+            if (!$z){
+                //show_bug($model -> getError());
+                $this->error("您录入的数据格式错误！");
+                exit();
+            }
+
+            if ($model->find(I("post.pk")) == null){
+                //错误ID
+                $this->error("页面无法访问！");
+            }else{
+                //处理信息
+                if ($_FILES['photo']['name']==''){
+                    //文件未上传
+                }else{
+                    //有修改文件上传
+
+                    //上传照片处理
+                    $upload = new \Think\Upload();// 实例化上传类
+                    $upload->maxSize   =     3145728 ;// 设置附件上传大小
+                    $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+                    $upload->rootPath  =      'CDN/uploaded/person/'; // 设置附件上传根目录
+                    $upload->autoSub  =      true;
+                    // 上传单个文件
+                    $info   =   $upload->uploadOne($_FILES['photo']);
+                    if(!$info) {// 上传错误提示错误信息
+                        $this->error($upload->getError());
+                    }else{// 上传成功 获取上传文件信息
+                        $photo =  $info['savepath'].$info['savename'];
+                        $thumb = $info['savepath'].'thumb_'.$info['savename'];
+
+                        //缩略图处理
+                        $image = new \Think\Image();
+                        $image->open('CDN/uploaded/person/'.$photo);
+                        // 生成缩略图
+                        $image->thumb(110, 90)->save('CDN/uploaded/person/'.$thumb);
+                        $data["photo"] = $photo;
+                        $data["thumbnail"] = $thumb;
+                    }
+                }
+
+                //数据修改
+
+                $data["update_time"] = date("Y-m-d H:i:s");
+                $data["regenerator"] = session("a_username");
+                $data["allow_copy"] = checkBit(I('post.allow_copy'));
+                $data["author"] = I("post.author");
+                $data["content"] = I("post.content");
+                $data["custom_sort"] = I("post.custom_sort");
+                $data["keywords"] = I("post.keywords");
+                $data["photo_describe"] = I("post.photo_describe");
+                $data["summary"] = I("post.summary");
+                $data["title"] = I("post.title");
+
+                $model->  where("pk=".I('post.pk'))  ->setField($data);
+                //录入成功
+                $this->success("恭喜您，操作成功！",__MODULE__."/Cms/personList");
+            }
+
+
+        }else{
+            $a = M("person_intro");
+            if ($a->find(I("get.pa")) == null){
+                //错误ID
+                $this->error("页面无法访问！");
+            }else{
+                //修改界面展示
+                $info = $a -> where("pk=".I("get.pa")) -> select();
+                $this -> assign("info",$info);
+                $this -> display();
+            }
+        }
+    }
+
+    //查看
+    function personShow(){
+        $user = M("person_intro");
+
+        if ($user->find(I("get.pa")) == null){
+            //错误ID
+            $this->error("页面无法访问！");
+        }else{
+            //界面展示
+            $info = $user -> where("pk=".I("get.pa")) -> select();
+            $this -> assign("info",$info);
+            $this -> display();
+        }
+
+    }
+
+//**********技术团队管理************
+    //列表
+    function storyList(){
+        $user = M("picture_story");
+        $info = $user ->order("is_show desc,custom_sort desc,update_time desc") -> select();
+        $this -> assign("info",$info);
+        $this -> display();
+    }
+
+    //过时与恢复
+    function storyLocked(){
+        if (I('get.sa')=="lock"){
+            $str = "恭喜您，记录过时成功！";
+            $sa = false;
+        }else{
+            $str = "恭喜您，记录恢复成功！";
+            $sa = true;
+        }
+        $model = M("picture_story");
+        $data = array('is_show'=> $sa,'regenerator'=>session("a_username"),'update_time'=>date("Y-m-d H:i:s"));
+        $model-> where('pk='.I('get.pa'))->setField($data);
+
+        //操作成功
+        $this->success($str,__MODULE__."/Cms/storyList");
+    }
+
+    //新增
+    function storyAdd(){
+        if (!empty($_POST)){
+            //实例化
+            $model = new \Model\Picture_storyModel();
+            //验证数据 Picture_storyModel
+            $z = $model -> create();
+            if (!$z){
+                //show_bug($model -> getError());
+                $this->error("您录入的数据格式错误！");
+                exit();
+            }
+
+
+            //上传照片处理
+            $upload = new \Think\Upload();// 实例化上传类
+            $upload->maxSize   =     3145728 ;// 设置附件上传大小
+            $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+            $upload->rootPath  =      'CDN/uploaded/person/'; // 设置附件上传根目录
+            $upload->autoSub  =      true;
+            // 上传单个文件
+            $info   =   $upload->uploadOne($_FILES['photo']);
+            if(!$info) {// 上传错误提示错误信息
+                $this->error($upload->getError());
+            }else{// 上传成功 获取上传文件信息
+                $photo =  $info['savepath'].$info['savename'];
+                $thumb = $info['savepath'].'thumb_'.$info['savename'];
+
+                //缩略图处理
+                $image = new \Think\Image();
+                $image->open('CDN/uploaded/person/'.$photo);
+                // 生成缩略图
+                $image->thumb(110, 90)->save('CDN/uploaded/person/'.$thumb);
+            }
+
+            //数据录入
+            $log["create_time"] = date("Y-m-d H:i:s");
+            $log["creator"] = session("a_username");
+            $log["update_time"] = date("Y-m-d H:i:s");
+            $log["regenerator"] = session("a_username");
+            $log["allow_copy"] = checkBit(I('post.allow_copy'));
+            $log["author"] = I("post.author");
+            $log["content"] = I("post.content");
+            $log["custom_sort"] = I("post.custom_sort");
+            $log["keywords"] = I("post.keywords");
+            $log["photo_describe"] = I("post.photo_describe");
+            $log["is_show"] = true;
+            $log["summary"] = I("post.summary");
+            $log["title"] = I("post.title");
+            $log["browse_count"] = 0;
+            $log["photo"] = $photo;
+            $log["thumbnail"] = $thumb;
+            $model->data($log)->add();
+
+            //录入成功
+            $this->success("恭喜您，操作成功！",__MODULE__."/Cms/storyList");
+
+        }else{
+            $this -> display();
+        }
+    }
+
+
 }
---
+++ Model/Person_introModel.class.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Model;
+use Think\Model;
+class Person_introModel extends Model{
+    //获取全部验证错误
+    protected $patchValidate = true;
+
+    //表单验证
+    protected $_validate = array(
+        //验证标题
+        array("title","require","标题必须填写"),
+        array('title','2,90','标题长度必须在2到90之间！',3,'length'),
+        array('title', '/^[0-9a-zA-Z\x{4e00}-\x{9fa5}]+$/u' , '标题由中英文和数字组成' , 3, 'regex'),
+
+        //验证摘要简介
+        array('summary','0,200','简介长度必须在0到200之间！',3,'length'),
+
+        //验证自定义排序
+        array("custom_sort","require","自定义排序必须填写"),
+        array('custom_sort','1,10','自定义排序长度必须在1到10之间！',3,'length'),
+        array('custom_sort', '/^[1-9][0-9]*$/' , '自定义排序必须是正整数' , 3, 'regex'),
+
+        //照片描述
+        array("photo_describe","require","照片描述不能为空"),
+        array('photo_describe','1,20','照片描述长度必须在1到20之间！',3,'length'),
+
+        //成员照片
+        array("photo","require","请上传成员照片"),
+
+        //关键词
+        array('keywords','0,200','关键词长度必须在0到200之间！',3,'length'),
+
+    );
+
+}
