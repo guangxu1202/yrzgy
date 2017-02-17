837300602feb1032511976c397f6cf6cda1ce861
guangxu1202 <guangxu1202@sina.com>
2017-02-17 17:35:54 +0800

    company 0217

--- House/Controller/FilesController.class.php
+++ House/Controller/FilesController.class.php
@@ -27,27 +27,31 @@ class FilesController extends CommonController {
 
     }
     function  storyCover(){
+        $upload = new \Think\Upload();// 实例化上传类
+        $info   =   $upload->upload();
+        echo "123333";
         // Define a destination
-        $targetFolder = '/CDN/up/'; // Relative to the root
+//        $targetFolder = '/CDN/up/'; // Relative to the root
+//
+//        $verifyToken = md5('unique_salt' . $_POST['timestamp']);
 
-        $verifyToken = md5('unique_salt' . $_POST['timestamp']);
 
-        if (!empty($_FILES) && $_POST['token'] == $verifyToken) {
-            $tempFile = $_FILES['Filedata']['tmp_name'];
-            $targetPath = $_SERVER['DOCUMENT_ROOT'] . $targetFolder;
-            $targetFile = rtrim($targetPath,'/') . '/' . $_FILES['Filedata']['name'];
-
-            // Validate the file type
-            $fileTypes = array('jpg','jpeg','gif','png'); // File extensions
-            $fileParts = pathinfo($_FILES['Filedata']['name']);
-
-            if (in_array($fileParts['extension'],$fileTypes)) {
-                move_uploaded_file($tempFile,$targetFile);
-                echo '1';
-            } else {
-                echo 'Invalid file type.';
-            }
-        }
+//        if (!empty($_FILES) && $_POST['token'] == $verifyToken) {
+//            $tempFile = $_FILES['Filedata']['tmp_name'];
+//            $targetPath = $_SERVER['DOCUMENT_ROOT'] . $targetFolder;
+//            $targetFile = rtrim($targetPath,'/') . '/' . $_FILES['Filedata']['name'];
+//
+//            // Validate the file type
+//            $fileTypes = array('jpg','jpeg','gif','png'); // File extensions
+//            $fileParts = pathinfo($_FILES['Filedata']['name']);
+//
+//            if (in_array($fileParts['extension'],$fileTypes)) {
+//                move_uploaded_file($tempFile,$targetFile);
+//                echo '1';
+//            } else {
+//                echo 'Invalid file type.';
+//            }
+//        }
     }
 
 }
