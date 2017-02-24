1ea8a565f44a05efedb46c1ae8ea555d15aa0408
guangxu1202 <guangxu1202@sina.com>
2017-02-24 17:34:04 +0800

    company 0224

---
+++ Home/Controller/CommonController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class CommonController extends Controller {
+    public function _initialize(){
+        // 自动运行方法
+        //获取导航
+//        $model = M("model");
+//        $nav = $model->field("name,pk") -> where("is_show = 1 and custom_sort >0") ->order("custom_sort desc") -> select();
+//        $this -> assign("nav",$nav);
+    }
+}
